bc3a5dc9efa173a5452333fd1c5dead995938995
Arch-W <alexs.santosmatos@gmail.com>
2022-12-08 17:21:32 +0000

    organization

--- script.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "http://localhost:8060/IntrusionManagementAPI/intrusion/video"
-files = {'upload_file': open('people-detection.mp4','rb')}
-values = {'DB': 'photcat', 'OUT': 'csv', 'SHORT': 'short'}
-r = requests.post(url, files=files, data=values)
